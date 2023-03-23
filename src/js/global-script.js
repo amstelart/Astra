b7e1fc22d1a11053316dbcbc34b126cb0a0e22c2
amstelart <amsteldroid@gmail.com>
2023-03-23 19:01:09 +0300

    03 commit

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -4,6 +4,18 @@ $( document ).ready(function() {
   $('.accordion-group').ariaAccordion({});
 
   $('.tab-group').ariaTabs({});
+
+  $('.v-tab-group').ariaTabs({
+    tabGroupIdPrefix: 'v-tab-group--',
+    navClass: 'v-tab-group__tab-nav',
+    listClass: 'v-tab-group__tab-ul',
+    listItemClass: 'v-tab-group__tab-li',
+    btnClass: 'v-tab-group__tab-btn',
+    panelsContainerClass: 'v-tab-group__tabs-cont',
+    panelClass: 'v-tab-group__tabpanel',
+    contentClass: 'v-tab-group__tab-content',
+    btnSelectedClass: 'v-tab-group__tab-btn_selected'
+  });
 });
 
 // Изоляция без jQuery
