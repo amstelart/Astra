9c51bd69d1ab5c7a49d89eda58b8941bdf48dfed
amstelart <amsteldroid@gmail.com>
2023-04-19 17:21:56 +0300

    15 commit

--- src/blocks/main-nav/main-nav.js
+++ src/blocks/main-nav/main-nav.js
@@ -60,16 +60,16 @@
   };
 
   // Добавление метода .closest() (полифил, собственно)
-  // (function(e){
-  //  e.closest = e.closest || function(css){
-  //    var node = this;
+  (function(e){
+   e.closest = e.closest || function(css){
+     var node = this;
 
-  //    while (node) {
-  //       if (node.matches(css)) return node;
-  //       else node = node.parentElement;
-  //    }
-  //    return null;
-  //  }
-  // })(Element.prototype);
+     while (node) {
+        if (node.matches(css)) return node;
+        else node = node.parentElement;
+     }
+     return null;
+   }
+  })(Element.prototype);
 
 }());
